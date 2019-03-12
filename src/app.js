959967678e201cb9c587db4b475031919fc80b7f
ivan <iv.dovg@gmail.com>
2019-03-12 23:18:01 +0500

    external configuring loggers

--- src/app.js
+++ src/app.js
@@ -6,15 +6,29 @@ import createAccessLogMiddleware from './middleware/accessLog';
 import createRequestIdMiddleware from './middleware/requestId';
 import createErrorLogMiddleware from './middleware/errorLog';
 import createRouter from './route';
+import * as MathService from './service/math';
 
-export default ({ logger }: { logger: Logger }) => {
+export interface AppOptions {
+  logger: Logger,
+  appLogger?: Logger,
+  accessLogger?: Logger,
+  errorLogger?: Logger,
+}
+
+export default ({
+  logger,
+  appLogger = logger.child({}),
+  accessLogger = logger.child({}),
+  errorLogger = logger.child({}),
+}: AppOptions) => {
   const app = express();
-  app.locals.logger = logger;
+  app.locals.logger = appLogger;
+  app.locals.mathService = MathService.produce();
   app.use(
     createRequestIdMiddleware(),
-    createAccessLogMiddleware({ logger }),
+    createAccessLogMiddleware({ logger: accessLogger }),
     createRouter(),
-    createErrorLogMiddleware({ logger }),
+    createErrorLogMiddleware({ logger: errorLogger }),
   );
   return app;
 };
