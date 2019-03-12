a36698419290c35b8c0cdeaaeb12a23e2372aeed
ivan <iv.dovg@gmail.com>
2019-03-13 04:26:37 +0500

    add negate integration test

--- src/route/math/__itests__/divide.js
+++ src/route/math/__itests__/divide.js
@@ -3,13 +3,34 @@
 import request from 'supertest';
 import responseSchema from '../schema/devision/response';
 
-describe('POST /user', () => {
-  it('user.name should be an case-insensitive match for "john"', (done) => {
+describe('GET /match', () => {
+  it('response should valid schema', (done) => {
     request(APP)
       .get('/math')
       .query({ a: 100, b: 5 })
       .set('Accept', 'application/json')
       .expect(validateSchema(responseSchema))
+      .end(done);
+  });
+  it('response should true calc', (done) => {
+    request(APP)
+      .get('/math')
+      .query({ a: 100, b: 5 })
+      .set('Accept', 'application/json')
+      .expect({
+        success: true,
+        data: {
+          value: 20
+        }
+      })
+      .end(done);
+  });
+  it('response should be bad request if query not valid', (done) => {
+    request(APP)
+      .get('/math')
+      .query({ a: 100 })
+      .set('Accept', 'application/json')
+      .expect(400)
       .end(done);
   });
 });
