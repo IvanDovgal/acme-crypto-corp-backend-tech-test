6b7259f8ac095c89b5a6904426123347e350d6c9
ivan <iv.dovg@gmail.com>
2019-03-12 22:37:52 +0500

    types annotations for postgresLogStream

--- src/misc/postgresLogStream/index.js
+++ src/misc/postgresLogStream/index.js
@@ -1,5 +1,8 @@
 // @flow
 
+import type { Client } from 'pg';
+import type { BunyanRecord, Writable } from 'bunyan';
+
 const createLogTableStatement = ({ tableName }) => `
   CREATE TABLE IF NOT EXISTS ${tableName} (
     id SERIAL,
@@ -10,18 +13,31 @@ const createLogTableStatement = ({ tableName }) => `
 const createInsertStatement = ({ tableName }) => `INSERT INTO ${tableName}(log) VALUES ($1)`;
 
 class PostgresLogStream {
+
+  client: Client;
+  statement: string;
+
   constructor({ tableName, client }) {
     this.client = client;
     this.statement = createInsertStatement({ tableName });
   }
 
-  write(data) {
-    this.client.query(this.statement, [data]);
+  write(data: BunyanRecord) {
+    this.client.query(this.statement, [ data ]);
   }
 }
 
-export const createPostgresLogStreamAsync = async ({ tableName = 'log', autoCreate = true, client }) => {
-  if(autoCreate)
-    await client.query(createLogTableStatement({ tableName }));
+export type PostgresLogStreamOptions = {
+  tableName?: string,
+  autoCreate?: boolean,
+  client: Client,
+}
+
+export const createPostgresLogStreamAsync = async ({
+  tableName = 'log',
+  autoCreate = true,
+  client,
+}: PostgresLogStreamOptions): Promise<Writable> => {
+  if (autoCreate) await client.query(createLogTableStatement({ tableName }));
   return new PostgresLogStream({ tableName, client });
 };
