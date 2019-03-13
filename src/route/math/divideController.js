71d553f619573ff22982228ae22502ed0d9d4cb4
ivan <iv.dovg@gmail.com>
2019-03-13 05:22:43 +0500

    fix test

--- src/route/math/divideController.js
+++ src/route/math/divideController.js
@@ -4,7 +4,7 @@ import { validate } from 'express-jsonschema';
 import type { TrackedRequest, TrackedResponse } from '../../types';
 import querySchema from './schema/division/query';
 
-export default [validate({ bodye: querySchema }), (req: TrackedRequest, res: TrackedResponse) => {
+export default [validate({ query: querySchema }), (req: TrackedRequest, res: TrackedResponse) => {
   const a = Number(Array.isArray(req.query.a) ? req.query.a[0] : req.query.a);
   const b = Number(Array.isArray(req.query.b) ? req.query.b[0] : req.query.b);
   res.status(200).json({
